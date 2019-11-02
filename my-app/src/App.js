d632185121e4fb730c3cd21fc8ed378c997c99f6
HereticHorse <leena.karjalainen92@gmail.com>
2019-11-02 15:25:07 +0200

    lets make a mess :+1:

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
+
 import "./App.css";
-//import Greet from "./component/appPage";
-//import Headers from "./component/Headers";
+import Greet from "./component/appPage";
+import Navbar from "./component/Navbar";
 import Message from "./component/message";
 import Counter from "./component/Counter";
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className="App">
-				{/* <Headers name="Laa" heroName="Batman" /> */}
+			<div class="header" className="App">
+				<Navbar />
 				<Message></Message>
 				<Counter></Counter>
 
-				{/* <Greet name="Laa" heroName="Batman">
+				<Greet name="Laa" heroName="Batman">
 					<p>This is childern props</p>
 				</Greet>
 				<Greet name="Lii" heroName="Wonder woman">
 					<button> fly away!</button>
 				</Greet>
-				<Greet name="Luu" heroName="Iron Man" /> */}
+				<Greet name="Luu" heroName="Iron Man" />
 			</div>
 		);
 	}
